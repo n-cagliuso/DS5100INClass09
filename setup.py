0918b085f60ce6821b81606faed9b35a47537984
NickVincent_Cagliuso <nph4zk@virginia.edu>
2023-03-28 20:24:13 -0400

    Add

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name = 'HelloPackage', version = '1.0', description = 'A package that says hello', url = 'https://github.com/n-cagliuso/DS5100INClass09', packages = ['HelloPackage'])
